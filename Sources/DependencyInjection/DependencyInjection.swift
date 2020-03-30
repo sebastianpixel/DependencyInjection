c2682de1ac17c68d416202b71583723cc971f761
Sebastian Pickl <sebastian.pickl@sz.de>
2020-03-30 08:11:55 +0200

    adds DependencyInjection impl with tests

--- Sources/DependencyInjection/DependencyInjection.swift
+++ Sources/DependencyInjection/DependencyInjection.swift
@@ -1,3 +1,158 @@
-struct DependencyInjection {
-    var text = "Hello, World!"
+import Foundation
+
+@propertyWrapper
+public struct Inject<T> {
+    public let wrappedValue: T
+
+    public init() {
+        wrappedValue = DIContainer.shared.resolve(T.self)
+    }
+}
+
+@propertyWrapper
+public class LazyInject<T> {
+
+    private var value: T?
+
+    public var wrappedValue: T {
+        if let value = value {
+            return value
+        } else {
+            let value = DIContainer.shared.resolve(T.self)
+            self.value = value
+            return value
+        }
+    }
+
+    public init() {}
+}
+
+public protocol Resolver {
+    func resolve<T>() -> T
+    func resolve<T>(_: T.Type) -> T
+}
+
+public final class DIContainer {
+    private var registrations = [ObjectIdentifier: Registration]()
+    private var sharedInstances = [ObjectIdentifier: Any]()
+    private var typeAliases = [ObjectIdentifier: ObjectIdentifier]()
+
+    public static let shared = DIContainer()
+
+    private init() {}
+}
+
+extension DIContainer: Resolver {
+
+    public func resolve<T>() -> T {
+        resolve(T.self)
+    }
+
+    public func resolve<T>(_: T.Type) -> T {
+        let identifier = ObjectIdentifier(T.self)
+        let type = typeAliases[identifier] ?? identifier
+
+        guard let registration = registrations[type] else {
+            fatalError("Configuration error: No initializer registered for type \"\(T.self)\".")
+        }
+
+        if registration is New {
+            return initialize(registration.initializer)
+        } else {
+            if let sharedInstance = sharedInstances[type] as? T {
+                return sharedInstance
+            }
+            let instance = initialize(registration.initializer) as T
+            sharedInstances[type] = instance
+            return instance
+        }
+    }
+
+    private func initialize<T>(_ initializer: () -> Any) -> T {
+        let instance = initializer()
+        if let instance = instance as? T {
+            return instance
+        } else {
+            fatalError("Configuration error: Could not cast instance of type \"\(type(of: instance))\" to type \"\(T.self)\".")
+        }
+    }
+}
+
+extension DIContainer {
+
+    @_functionBuilder
+    public struct RegistrationBuilder {
+        public static func buildBlock(_ routes: Registration...) -> [Registration] {
+            routes
+        }
+    }
+
+    public func register(@RegistrationBuilder _ registrations: () -> [Registration]) {
+        registrations().forEach(register)
+    }
+
+    public func register(_ registration: Registration) {
+        registration.aliases?.forEach {
+            typeAliases[$0] = registration.identifier
+        }
+        self.registrations[registration.identifier] = registration
+    }
+}
+
+public protocol Registration {
+    var identifier: ObjectIdentifier { get }
+    var aliases: [ObjectIdentifier]? { get }
+    var initializer: () -> Any { get }
+
+    init<T>(typeAliases: [Any.Type]?, initializer: @escaping () -> T)
+}
+
+public extension Registration {
+    init<T>(_ initializer: @escaping () -> T) {
+        self.init(typeAliases: nil, initializer: initializer)
+    }
+
+    init<T>(_ initializer: @escaping @autoclosure () -> T) {
+        self.init(initializer)
+    }
+
+    init<T>(_ initializer: @escaping (Resolver) -> T) {
+        self.init { initializer(DIContainer.shared) }
+    }
+
+    init<T>(_ initializer: @escaping () -> T, as typeAliases: Any.Type...) {
+        self.init(typeAliases: typeAliases, initializer: initializer)
+    }
+
+    init<T>(_ initializer: @autoclosure @escaping () -> T, as typeAliases: Any.Type...) {
+        self.init(typeAliases: typeAliases, initializer: initializer)
+    }
+
+    init<T>(_ initializer: @escaping (Resolver) -> T, as typeAliases: Any.Type...) {
+        self.init(typeAliases: typeAliases) { initializer(DIContainer.shared) }
+    }
+}
+
+public struct New: Registration {
+    public let identifier: ObjectIdentifier
+    public let aliases: [ObjectIdentifier]?
+    public let initializer: () -> Any
+
+    public init<T>(typeAliases: [Any.Type]?, initializer: @escaping () -> T) {
+        self.identifier = ObjectIdentifier(T.self)
+        self.aliases = typeAliases?.map(ObjectIdentifier.init)
+        self.initializer = initializer
+    }
+}
+
+public struct Shared: Registration {
+    public let identifier: ObjectIdentifier
+    public let aliases: [ObjectIdentifier]?
+    public let initializer: () -> Any
+
+    public init<T>(typeAliases: [Any.Type]?, initializer: @escaping () -> T) {
+        self.identifier = ObjectIdentifier(T.self)
+        self.aliases = typeAliases?.map(ObjectIdentifier.init)
+        self.initializer = initializer
+    }
 }
--- Tests/DependencyInjectionTests/DependencyInjectionTests.swift
+++ Tests/DependencyInjectionTests/DependencyInjectionTests.swift
@@ -1,15 +1,56 @@
 import XCTest
 @testable import DependencyInjection
 
+protocol DummyProtocol: AnyObject {}
+
 final class DependencyInjectionTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(DependencyInjection().text, "Hello, World!")
+
+    private class DummyClass: DummyProtocol {}
+
+    @LazyInject private var dummy: DummyProtocol
+
+    func testSharedInstanceResolvesToIdenticalInstance() {
+        DIContainer.shared.register(Shared(DummyClass()))
+        XCTAssert(DIContainer.shared.resolve(DummyClass.self) === DIContainer.shared.resolve(DummyClass.self))
+    }
+
+    func testNewInstanceResolvesToDifferentInstance() {
+        DIContainer.shared.register(New(DummyClass()))
+        XCTAssert(DIContainer.shared.resolve(DummyClass.self) !== DIContainer.shared.resolve(DummyClass.self))
+    }
+
+    func testLazyInjectPropertyWrapperWithNewInstanceProtocolConformance() {
+        DIContainer.shared.register(New(DummyClass() as DummyProtocol))
+        XCTAssert(dummy !== DIContainer.shared.resolve(DummyProtocol.self))
+    }
+
+    func testLazyInjectPropertyWrapperWithSharedInstancesAndProtocolConformance() {
+        DIContainer.shared.register(Shared(DummyClass() as DummyProtocol))
+        XCTAssert(dummy === DIContainer.shared.resolve(DummyProtocol.self))
+    }
+
+    func testInjectPropertyWrapper() {
+        class DummyWithInjectedProperty {
+            @Inject var injectedProperty: DummyProtocol
+        }
+        DIContainer.shared.register(Shared(DummyClass() as DummyProtocol))
+        XCTAssert(DummyWithInjectedProperty().injectedProperty === DIContainer.shared.resolve(DummyProtocol.self))
+    }
+
+    func testOverrideRegistrationFromProductionInTests() {
+        DIContainer.shared.register(Shared(DummyClass() as DummyProtocol))
+        XCTAssert(DIContainer.shared.resolve(DummyProtocol.self) === DIContainer.shared.resolve(DummyProtocol.self))
+
+        DIContainer.shared.register(New(DummyClass() as DummyProtocol))
+        XCTAssert(DIContainer.shared.resolve(DummyProtocol.self) !== DIContainer.shared.resolve(DummyProtocol.self))
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testSharedInstanceResolvesToIdenticalInstance", testSharedInstanceResolvesToIdenticalInstance),
+        ("testNewInstanceResolvesToDifferentInstance", testNewInstanceResolvesToDifferentInstance),
+        ("testLazyInjectPropertyWrapperWithNewInstanceProtocolConformance", testLazyInjectPropertyWrapperWithNewInstanceProtocolConformance),
+        ("testLazyInjectPropertyWrapperWithSharedInstancesAndProtocolConformance", testLazyInjectPropertyWrapperWithSharedInstancesAndProtocolConformance),
+        ("testInjectPropertyWrapper", testInjectPropertyWrapper),
+        ("testOverrideRegistrationFromProductionInTests", testOverrideRegistrationFromProductionInTests)
     ]
 }
